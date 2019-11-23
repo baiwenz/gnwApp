a9ea4ce400bf62aa021dc3da2496f2317d1a8e32
15208654896 <1206834441@qq.com>
2019-11-23 16:09:22 +0800

    完成登录加载动画

--- mylibrary/src/main/java/com/example/mylibrary/fragment/Login/FragmentLogin.java
+++ mylibrary/src/main/java/com/example/mylibrary/fragment/Login/FragmentLogin.java
@@ -1,5 +1,6 @@
 package com.example.mylibrary.fragment.Login;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,18 +9,21 @@ import android.widget.TextView;
 
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.dd.processbutton.iml.ActionProcessButton;
 import com.example.commonlibrary.ARouter.Constance;
 import com.example.commonlibrary.ARouter.LoginNavigationCallbackImpl;
 import com.example.commonlibrary.base.BaseFragment;
 import com.example.commonlibrary.presenter.BasePresenter;
+import com.example.commonlibrary.utils.ProgressGenerator;
 import com.example.mylibrary.R;
 
-public class FragmentLogin extends BaseFragment implements View.OnClickListener {
+public class FragmentLogin extends BaseFragment implements View.OnClickListener, ProgressGenerator.OnCompleteListener {
     private LoginActivity loginActivity;
     private static FragmentLogin mFragmentLogin;
     private TextView mRegister;
     private TextView mForgotPwd;
-
+    private ActionProcessButton btnSignIn;
+    final ProgressGenerator progressGenerator = new ProgressGenerator(this);
     public static FragmentLogin newInstance(String param1) {
         mFragmentLogin = new FragmentLogin();
         Bundle args = new Bundle();
@@ -46,19 +50,31 @@ public class FragmentLogin extends BaseFragment implements View.OnClickListener 
         loginActivity=(LoginActivity) getActivity();
         mRegister = view.findViewById(R.id.tv_register);
         mForgotPwd = view.findViewById(R.id.tv_forgot_pwd);
+        btnSignIn = (ActionProcessButton) view.findViewById(R.id.btn_login);
     }
     @Override
     public void initData() {
         mRegister.setOnClickListener(this);
         mForgotPwd.setOnClickListener(this);
+        btnSignIn.setOnClickListener(this);
     }
 
+    private void setLoginBtnLoginLoading(){
+        btnSignIn.setMode(ActionProcessButton.Mode.ENDLESS);
+        progressGenerator.start(btnSignIn);
+    }
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.tv_register){
             loginActivity.showFragment(1);
         }else if(view.getId() == R.id.tv_forgot_pwd){
             loginActivity.showFragment(4);
+        }else if (view.getId()==R.id.btn_login){
+            setLoginBtnLoginLoading();
         }
+    }
+
+    @Override
+    public void onComplete() {
     }
 }
